6a1e5e296e1b23a45ba0edb54e525a411dd5623e
Konstantin Gazha <kgazha@gmail.com>
2024-08-13 21:51:05 +0500

    Связный список. Поиск и вставка

---
+++ ConsoleApp/Node.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp;
+
+public class Node<TValue>
+{
+    public TValue Value { get; }
+    public Node<TValue> Next { get; set; }
+
+    public Node(TValue value, Node<TValue> next)
+    {
+        Value = value;
+        Next = next;
+    }
+
+    public override string? ToString()
+    {
+        return Value?.ToString();
+    }
+}
+
+public static class CustomLinkedList
+{
+    public static Node<TValue> GetNodeByIndex<TValue>(Node<TValue> head, int index)
+    {
+        while (index != 0)
+        {
+            head = head.Next;
+            index--;
+        }
+
+        return head;
+    }
+
+    public static Node<TValue> InsertNode<TValue>(Node<TValue> head, int index, TValue value)
+    {
+        var newNode = new Node<TValue>(value, head);
+        if (index == 0)
+        {
+            return newNode;
+        }
+
+        var node = GetNodeByIndex(head, index - 1);
+        newNode.Next = node.Next;
+        node.Next = newNode;
+        return head;
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp;
+
+
+void NewNode()
+{
+    var node3 = new Node<string>("node3", null);
+    var node2 = new Node<string>("node2", node3);
+    var node1 = new Node<string>("node1", node2);
+    var node0 = new Node<string>("node0", node1);
+
+    var result = CustomLinkedList.GetNodeByIndex(node0, 1);
+    Console.WriteLine(result);
+    node0 = CustomLinkedList.InsertNode(node0, 1, "NewNode");
+    Console.WriteLine(node0.Next);
+}
+
